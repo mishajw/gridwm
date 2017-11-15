687ee50539630f055357250f9332f1a590293b8c
Misha Wagner <mishajw@gmail.com>
2017-11-15 20:42:27 +0000

    Add command line arguments for fifo paths

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
+extern crate clap;
+
 mod external_commands;
 mod fifo_reader;
 
 fn main() {
-    fifo_reader::read_loop("/tmp/gridwm_fifo");
+    let matches = clap::App::new("gridwm")
+        .arg(clap::Arg::with_name("fifo_path")
+            .long("fifo-path")
+            .short("f")
+            .help("The path of the fifo to read commands from")
+            .takes_value(true))
+        .get_matches();
+
+    let fifo_path = matches
+        .value_of("fifo_path")
+        .unwrap_or("/tmp/gridwm_fifo");
+
+    fifo_reader::read_loop(fifo_path);
 }
 
